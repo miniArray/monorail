a2926b4b80c5911bcb5bb8b51327a14d301407fe
Simon W. Jackson <simon@simonwjackson.com>
2018-03-28 08:39:59 -0500

    refactor: add px() to utils

--- src/utils/toolbelt/index.js
+++ src/utils/toolbelt/index.js
@@ -1,9 +1,11 @@
 import * as R from 'ramda'
 import * as RA from 'ramda-adjunct'
+const px = num => `${num}px`
 
 export default {
   ...R,
   ...RA,
   appendStr: R.flip(R.concat),
-  inList: R.flip(R.contains)
+  inList: R.flip(R.contains),
+  px
 }

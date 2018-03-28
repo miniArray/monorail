c203129a5e3f00a1a5d55af2b77f6af99235c024
Simon W. Jackson <simon@simonwjackson.com>
2018-03-28 08:39:00 -0500

    refactor: add navigation fixture

---
+++ src/fixtures/navigation.js
@@ -0,0 +1,39 @@
+export default {
+  items: [{
+    caption: 'Fruit',
+    items: [{
+      icon: '🍒',
+      caption: 'Cherries'
+    }, {
+      icon: '🍈',
+      caption: 'Melons'
+    }, {
+      icon: '🍓',
+      caption: 'Strawberries'
+    }, {
+      icon: '🍊',
+      caption: 'Tangerines'
+    }, {
+      icon: '🥝',
+      caption: 'Kiwis'
+    }, {
+      icon: '🍇',
+      caption: 'Grapes'
+    }]
+  }, {
+    caption: 'Veggies',
+    items: [{
+      icon: '🍅',
+      caption: 'Tomatoes'
+    }, {
+      icon: '🍆',
+      caption: 'Melons'
+    }, {
+      icon: '🥕',
+      caption: 'Carrots'
+    }, {
+      icon: '🥒',
+      caption: 'Cucumbers'
+    }]
+  }]
+}

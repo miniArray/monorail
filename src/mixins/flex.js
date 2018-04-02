29d8c68cde4cc57aaa6b21dd9246559c5d7a3469
Simon W. Jackson <simon@simonwjackson.com>
2018-04-01 19:28:38 -0500

    refactor(mixin): add flex mixin

---
+++ src/mixins/flex.js
@@ -0,0 +1,23 @@
+export default {
+  props: {
+    alignCenter: {
+      type: Boolean,
+      default: false
+    },
+
+    justifyCenter: {
+      type: Boolean,
+      default: false
+    },
+
+    fillHeight: {
+      type: Boolean,
+      default: false
+    },
+
+    fluid: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
